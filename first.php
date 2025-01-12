e4f7ae0ab6ef7a2e1b57d512be03e817494d574c
sameer vats <sameervats13@gmail.com>
2025-01-12 14:01:44 +0530

    vardump tell us variable datatype

--- first.php
+++ first.php
@@ -2,5 +2,12 @@
 $a=12;
 $b=15;
 $c=$a+$b;
-echo "sum of a and b is $c";
-?>
+$ambala=12.3;
+echo "<br>sum of a and b is $c";
+echo "<br>";
+$chd="pankaj";
+var_dump($a);
+echo "<br>";
+var_dump($chd); //vardump tell us datatype
+echo "<br>";
+var_dump($ambala);
