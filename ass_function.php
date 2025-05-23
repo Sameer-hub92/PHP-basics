45ccd41ccb57549795db3f3498e3b8c79505647d
sameer vats <sameervats13@gmail.com>
2025-05-23 10:38:29 +0530

    use more predefines method of associative array

--- ass_function.php
+++ ass_function.php
@@ -1,11 +1,26 @@
 <?php
 
 $array=array("Name"=>"Pankaj","Branch"=>"cse","Roll"=>211);
-$array2=array("Nickname"=>"Pankaj","Branch"=>"cse");
 
-$newarray=array_intersect($array,$array2);
+// array_push($array,"Department","IT");
+// $array2=array("Nickname"=>"Pankaj","Branch"=>"cse");
+// $array3=array("name"=>"Pankaj","Branch"=>"icse");
 
-print_r($newarray);
+// $newarray=array_intersect($array,$array2,$array3); // value based
+
+// $newarray=array_intersect_key($array,$array2);
+
+if(array_key_exists("branch",$array))
+{
+    echo "Key is present";
+}
+else{
+    echo "Key is not Present";
+}
+
+
+
+// print_r($array);
 
 
 ?>
